41538d4010795484723cbee230517815ce41eea5
linear21 <ngabriogans7@gmail.com>
2022-01-02 04:37:58 +0000

    Collect ccache

--- final.sh
+++ final.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-cd /tmp/rom
 
+cd /tmp/rom
+source build/envsetup.sh
+lunch octavi_garden-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 20G
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
+export TZ=UTC
+ccache -M 50G
 ccache -o compression=true
 ccache -z
 
-. build/envsetup.sh
-lunch nad_juice-userdebug
-mka nad -j8
+make(){
+    brunch octavi_garden-userdebug
+	zip=$(upload out/target/product/garden/*zip)
+	echo " "
+	echo "$zip"
+}
+
+upload(){
+        curl --upload-file $1 https://transfer.sh/ 
+}
+
+make &
+sleep 5200 && kill "$!"
+ccache -s
--- ziping.sh
+++ ziping.sh
@@ -2,9 +2,13 @@
 
 cd /tmp
 
-com ()
-{
-    tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
+# Compress function with pigz for faster compression
+com () 
+{ 
+    tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz ccache
 }
 
-time com ccache 1
+time com ccache 1 # Compression level 1, its enough
+mkdir -p ~/.config/rclone
+echo "$rclone_config" > ~/.config/rclone/rclone.conf # Write rclone config found from env variable, so that cloud storage can be used to upload ccache
+time rclone copy ccache.tar.gz drive:ccache -P
